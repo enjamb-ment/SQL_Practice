1325f5522066742e161c321ae7637723422bbf71
Matt Wilcox <mewazhere@gmail.com>
2024-06-03 13:19:49 -0500

    Add'l practice

--- Hackerrank/2 - Basic Join.sql
+++ Hackerrank/2 - Basic Join.sql
@@ -23,7 +23,25 @@ JOIN CITY AS city ON country.Code = city.CountryCode
 GROUP BY country.CONTINENT;
 
 -- The Report
-SELECT IF(Grades.grade >= 8,Students.Name, NULL ), Grades.grade, Students.Marks
+SELECT IF(Grades.grade >= 8,Students.Name, NULL), Grades.grade, Students.Marks
 FROM Grades
 JOIN Students ON (Students.Marks >= Grades.Min_Mark AND Students.Marks <= Grades.Max_Mark)
 ORDER BY Grades.grade DESC, Students.Name ASC;
+
+-- Top Competitors
+    -- Tables: [Hackers], [Difficulty], [Challenges], [Submissions]
+    -- Query: hacker_id and name of hackers who achieved full scores for more than one challenge, ordered in descending order of total challenges
+        -- need to create a way to check full_score: COUNT(full_score) ... Submissions.score = Difficulty.score AS full_score(...?)
+            -- GROUP BY hacker_id, 
+            -- ORDER BY COUNT(full_score) DESC, hacker.hacker_id ASC;
+SELECT h.hacker_id, h.name
+FROM Hackers AS h
+JOIN Submissions AS s ON s.hacker_id = h.hacker_id
+JOIN Challenges AS c ON s.challenge_id = c.challenge_id
+JOIN Difficulty AS d ON c.difficulty_level = d.difficulty_level AND s.score = d.score
+--WHERE COUNT(s.score = d.score) AS full_score >= 1  -- Nope, I want the groups to have the condition; use HAVING
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(c.challenge_id) > 1 -- A little confused by this part.  I'm just counting the Challenges IDs.  
+                                 --   I think the JOIN Difficulty table line is what does it: 
+                                 --   It's joined on both the difficulty_level and the score (comparing the hacker's score to what's required for full score)
+ORDER BY COUNT(c.challenge_id) DESC, h.hacker_id ASC;
